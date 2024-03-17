ed010b91e492725141771f101cca1a44d5b4c551
Sissaz <siciiliagiacomazza@gmail.com>
2024-03-16 21:04:07 -0300

    update

--- pipeline_etl.py
+++ pipeline_etl.py
@@ -33,18 +33,29 @@ def listar_arquivos_csv(diretorio):
     print("Listagem concluída.")
     return arquivos_csv
 
+def is_utf8(filename):
+    try:
+        with open(filename, encoding='utf-8', errors='strict') as f:
+            f.read()
+        return True
+    except UnicodeDecodeError:
+        return False
+
 
 # Função para converter arquivos CSV para UTF-8
 def converter_arquivos_para_utf8(diretorio, max_linhas=8000):
-    arquivos_csv = [arquivo for arquivo in os.listdir(diretorio) if arquivo.endswith('.csv') and not arquivo.endswith('_utf8.csv')]
-    for arquivo in tqdm(arquivos_csv, desc="Convertendo arquivos para UTF-8"):
-        nome_arquivo_sem_extensao = arquivo.split('.')[0]
-        caminho_arquivo_origem = os.path.join(diretorio, arquivo)
-        caminho_arquivo_destino = os.path.join(diretorio, f"{nome_arquivo_sem_extensao}_utf8.csv")
-        # Se necessário, altere o encoding para o do arquivo csv utilizado. 
-        # Algumas opções comuns incluem: 'UTF-8', 'ISO-8859-1' (latin1), 'windows-1252', 'UTF-16', 'ASCII', 'cp1251' (windows-1251), 'gbk'.
-        df = pd.read_csv(caminho_arquivo_origem, sep=';', encoding='ISO-8859-1', on_bad_lines='skip', nrows=max_linhas)
-        df.to_csv(caminho_arquivo_destino, sep=';', index=False, encoding='utf-8')
+    arquivos_csv = [arquivo for arquivo in os.listdir(diretorio) if arquivo.endswith('.csv')]
+    for arquivo in tqdm(arquivos_csv, desc="Verificando e convertendo arquivos para UTF-8"):
+        caminho_arquivo = os.path.join(diretorio, arquivo)
+        if not is_utf8(caminho_arquivo):
+            nome_arquivo_sem_extensao = arquivo.split('.')[0]
+            caminho_arquivo_destino = os.path.join(diretorio, f"{nome_arquivo_sem_extensao}_utf8.csv")
+            # Se necessário, altere o encoding para o do arquivo csv utilizado. 
+            # Algumas opções comuns incluem: 'UTF-8', 'ISO-8859-1' (latin1), 'windows-1252', 'UTF-16', 'ASCII', 'cp1251' (windows-1251), 'gbk'.
+            df = pd.read_csv(caminho_arquivo, sep=';', encoding='ISO-8859-1', on_bad_lines='skip', nrows=max_linhas)
+            df.to_csv(caminho_arquivo_destino, sep=';', index=False, encoding='utf-8')
+        else:
+            print(f" Arquivo {arquivo} já está em UTF-8.")
 
 
 # Função para ler arquivos CSV e retornar um dataframe DuckDB
@@ -76,15 +87,23 @@ def transformar(df: DuckDBPyRelation) -> DataFrame:
     # Executa a consulta SQL que inclui as novas colunas
     df_transformado = duckdb.sql("""
         SELECT 
-            codigo_viagem, cnpj, numero_bilhete, data_emissao_bilhete, categoria_transporte, nu_linha,
-            tipo_servico, data_viagem, hora_viagem, tipo_viagem, numero_poltrona, plataforma_embarque, origem_emissao,
+            CONCAT(codigo_viagem, '-', numero_bilhete, '-', numero_poltrona, '-', plataforma_embarque) AS codigo,
+            CAST(codigo_viagem AS VARCHAR) AS codigo_viagem,
+            CAST(cnpj AS VARCHAR) AS cnpj,
+            CAST(numero_bilhete AS VARCHAR) AS numero_bilhete,                  
+            CAST(categoria_transporte AS VARCHAR) AS categoria_transporte,
+            CAST(nu_linha AS VARCHAR) AS nu_linha,
+            CAST(tipo_servico AS VARCHAR) AS tipo_servico,              
+            CAST(tipo_viagem AS VARCHAR) AS tipo_viagem,
+            CAST(numero_poltrona AS VARCHAR) AS numero_poltrona,
+            CAST(plataforma_embarque AS VARCHAR) AS plataforma_embarque,
+            CAST(plataforma_embarque AS VARCHAR) AS origem_emissao,
             REPLACE(CAST(valor_tarifa AS VARCHAR), '.', ',') AS valor_tarifa, 
             REPLACE(CAST(valor_percentual_desconto AS VARCHAR), '.', ',') AS valor_percentual_desconto, 
             REPLACE(CAST(valor_aliquota_icms AS VARCHAR), '.', ',') AS valor_aliquota_icms, 
             REPLACE(CAST(valor_pedagio AS VARCHAR), '.', ',') AS valor_pedagio, 
             REPLACE(CAST(valor_taxa_embarque AS VARCHAR), '.', ',') AS valor_taxa_embarque, 
             REPLACE(CAST(valor_total AS VARCHAR), '.', ',') AS valor_total,                              
-            CONCAT(codigo_viagem, '-', numero_bilhete, '-', numero_poltrona, '-', plataforma_embarque) AS codigo,
             CONCAT(TRIM(ponto_origem_viagem), ' - ', TRIM(ponto_destino_viagem)) AS origem_destino,
             TRIM(ponto_origem_viagem) as origem_viagem,
             TRIM(ponto_destino_viagem) as destino_viagem,
@@ -92,8 +111,8 @@ def transformar(df: DuckDBPyRelation) -> DataFrame:
                 WHEN in_passagem_cancelada = 'NÃO' THEN 'Não' 
                 ELSE 'Sim' 
             END AS cancelada,
-            SUBSTRING(data_viagem, 1, 5) AS dia_mes_viagem,  -- Extrai os 5 primeiros caracteres (DD-MM)
-            SUBSTRING(data_viagem, 4, 7) AS mes_viagem, -- Extrai os últimos 7 caracteres (MM-YYYY)
+            STRPTIME(CAST(data_emissao_bilhete AS VARCHAR), '%d-%m-%Y') AS data_emissao_bilhete,
+            STRPTIME(CAST(data_viagem AS VARCHAR), '%d-%m-%Y') AS data_viagem,
             SPLIT_PART(tipo_gratitude, ' - ', 1) AS tipo_gratuidade, -- Extrai texto antes do delimitador " - "
             CASE 
                 WHEN data_viagem = '02-11-2019' THEN 'Finados'
@@ -117,19 +136,28 @@ def salvar_postgres(df_duckdb, tabela):
 
 
 # Script principal
-if __name__ == "__main__":
+def pipeline():
+
     print("Iniciando o script...")
     url_pasta = 'https://drive.google.com/drive/folders/xxxxxxxxxxxxxxxxx'
     diretorio_local = './pasta_gdown'
     # Para fazer download dos arquivos, descomente o código abaixo
     # download_arquivos_gdrive(url_pasta, diretorio_local)
+
     converter_arquivos_para_utf8(diretorio_local)
     arquivos = listar_arquivos_csv(diretorio_local)
     ler_csv(arquivos)
     df_concatenado = juntar_arquivos_csv(diretorio_local, 'base_viagens.csv')
     df_transformado = transformar(df_concatenado)  # Aplica a transformação ao DataFrame concatenado
     caminho_arquivo_saida = os.path.join(diretorio_local, 'base_viagens.csv')
+    
     df_transformado.to_csv(caminho_arquivo_saida, sep=';', index=False, encoding='utf-8')  # Salva o DataFrame transformado no arquivo CSV
     print("Base salva localmente.")
+
     salvar_postgres(df_transformado, 'base_viagens')  # Salva o DataFrame transformado no PostgreSQL
     print("Base salva no PostgreSQL.")
+
+
+
+if __name__ == "__main__":
+    pipeline()
