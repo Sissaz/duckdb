d5a6df48b2f2ec4afa9ba123400d4238e2af2ec7
Sissaz <siciiliagiacomazza@gmail.com>
2024-03-17 21:29:09 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## Duckdb
+
+Pontos de atenção identificados com o DuckDB:
+
+- As colunas de data devem estar no formato ISO 8601;
+- Os arquivos devem estar codificados em UTF-8;
---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from pipeline_etl import pipeline
+
+st.title('Processador de Arquivos')
+
+if st.button('Processar'):
+    with st.spinner('Processando...'):
+        logs = pipeline()
+        # Exibe os logs no Streamlit
+        for log in logs:
+            st.write(log)
+
+#streamlit run app.py
